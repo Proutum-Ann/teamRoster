389c6f07009783bd6376946fd00491b16d958121
Proutum-Ann <526ALiang@frhsd.com>
2025-05-20 11:31:53 -0400

    styled modal card bg

--- js/roster.js
+++ js/roster.js
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         <!-- Modal -->
                         <div class="modal fade" id="${c.name}Modal" tabindex="-1" aria-hidden="true">
                         <div class="modal-dialog modal-dialog-centered">
-                            <div class="modal-content">
+                            <div class="modal-content ${c.au}">
                                 <div class="modal-header">
                                     <h1 class="modal-title fs-5" id="${c.name}ModalLabel">Additional Information on ${c.name}</h1>
                                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
